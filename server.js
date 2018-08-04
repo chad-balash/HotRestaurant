a018d7fea9965e8b8755a990cf3f9dc2439473c5
Christie Hennes <christiehennes@gmail.com>
2018-08-04 10:42:27 -0700

    Adding initial server file

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
