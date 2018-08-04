8ed9d03022e7bb9851e6939edef6231d94b98705
rduon002 <36011105+rduon002@users.noreply.github.com>
2018-08-04 11:09:14 -0700

    Add files via upload
    first commit

--- server.js
+++ server.js
@@ -11,72 +11,25 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Storing all the tables
+var data = {
+    reservations: [],
+    waitlist: [],
+};
 
-// Variables
-// =============================================================
+let visitorCount = 0;
 
-let tablesArray = [];
-let waitlistArray = [];
-
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+// Routing
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+    visitorCount++;
 });
 
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tabales.html"));
-});
-
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-
-
-// Displays all people on the waitlist
-app.get("/api/waitlist", function(req, res) {
-    return res.json(waitlistArray);
 });
 
-// Displays all tables
-app.get("/api/tables", function(req, res) {
-    return res.json(tablesArray);
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.post("/api/tables", function(req, res){
-
-    let newTable = req.body;
-
-  
-    console.log(newTable);
-  
-    tablesArray.push(newTable);
-  
-    res.json(newTable);
-
-});
-
-// Create New Characters - takes in JSON input
-app.post("/api/waitlist", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    let newWaitlistTable = req.body;
-
-  
-    console.log(newWaitlistTable);
-  
-    waitlistArray.push(newWaitlistTable);
-  
-    res.json(newWaitlistTable);
-
-  });
-  
-
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
